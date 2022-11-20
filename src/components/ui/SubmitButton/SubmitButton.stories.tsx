f49bd5e4035e79067b31c344970b11345f990583
Okkun555 <y.okuno.btm@gmail.com>
2022-11-20 12:14:28 +0900

    feat:SubmitButtonのUIカタログを追加

---
+++ src/components/ui/SubmitButton/SubmitButton.stories.tsx
@@ -0,0 +1,28 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SubmitButtonComponent from './SubmitButton';
+
+export default {
+  title: 'SubmitButton',
+  component: SubmitButtonComponent,
+} as ComponentMeta<typeof SubmitButtonComponent>;
+
+const Template: ComponentStory<typeof SubmitButtonComponent> = (args) => (
+  <SubmitButtonComponent
+    text={args.text}
+    color={args.color}
+    size={args.size}
+    width={args.width}
+    onClick={args.onClick}
+  />
+);
+
+export const SubmitButton = Template.bind({});
+SubmitButton.args = {
+  text: 'Sign In',
+  color: 'primary',
+  size: 'lg',
+  width: 200,
+  onClick: () => {
+    return;
+  },
+};
