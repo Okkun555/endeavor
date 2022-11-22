09e41140272b4a997beacc947ad0c2b5967564b7
Okkun555 <y.okuno.btm@gmail.com>
2022-11-22 23:44:52 +0900

    feat:認証フォームデザインをPC/SPで切り替え

--- src/components/features/auth/AuthForm/AuthForm.tsx
+++ src/components/features/auth/AuthForm/AuthForm.tsx
@@ -2,22 +2,24 @@ import { FC, useCallback } from 'react';
 import './AuthForm.css';
 import { AuthType } from '../types';
 import SubmitButton from '../../../ui/SubmitButton/SubmitButton';
+import { useDevice } from '../../../../hooks/useDevice';
 
 type PropsType = {
   formType: AuthType;
 };
 
 const AuthForm: FC<PropsType> = ({ formType }) => {
+  const { isMobile } = useDevice();
+
   const getFormTitle = useCallback(() => {
     return formType === 'signIn' ? 'Sign In' : 'Login';
   }, []);
 
   return (
     <div className="auth-form-container">
-      <form className="auth-form">
+      <form className={isMobile ? 'auth-form-mobile' : 'auth-form'}>
         <div className="auth-form-content">
           <h3 className="auth-form-title">{getFormTitle()}</h3>
-
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
@@ -26,7 +28,6 @@ const AuthForm: FC<PropsType> = ({ formType }) => {
               placeholder="Enter email"
             />
           </div>
-
           <div className="form-group mt-3">
             <label>Password</label>
             <input
@@ -35,7 +36,6 @@ const AuthForm: FC<PropsType> = ({ formType }) => {
               placeholder="Enter Password"
             />
           </div>
-
           <div className="d-grid gap-2 mt-4">
             <SubmitButton
               text={getFormTitle()}
