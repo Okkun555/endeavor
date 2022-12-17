08373387e1079cd30d7c7add9f79ba8e37d0902a
Okkun555 <y.okuno.btm@gmail.com>
2022-12-17 13:05:31 +0900

    feat:カラーコードを追加

--- src/config/color.ts
+++ src/config/color.ts
@@ -1,11 +1,16 @@
 // 通常系
 export const Main = '#ff4500';
 export const White = '#ffffff';
+export const Gray01 = '#d2d3da';
+export const Gray02 = '#b6b7be';
+export const Gray03 = '#95969e';
+export const Gray04 = '#787982';
+export const Gray05 = '#585963';
 
 // ボタン系
 export const HoverButton = '#ff5500';
 export const DisableText = '#d2d5db';
-export const DisableButton = '#6c7589';
+export const Disable = '#6c7589';
 
 // アラート系
 export const ERROR = '';
